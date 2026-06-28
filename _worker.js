bc40424749b016cf47d3ae9876f3e2d53a9355aa
IgonWBH <1605706464@qq.com>
2026-06-28 15:30:28 +0800

    修复 _worker.js 无限重定向循环

--- _worker.js
+++ _worker.js
@@ -3,16 +3,16 @@ export default {
         const url = new URL(request.url);
         const path = url.pathname;
 
-        // 如果是 /player/ 开头的请求，返回 player/index.html
-        if (path.startsWith('/player/')) {
-            // 构造一个指向 /player/index.html 的新请求
+        // 如果是 /player/ 开头的请求，且不是 /player/index.html 本身
+        if (path.startsWith('/player/') && !path.endsWith('/index.html')) {
+            // 重写请求到 /player/index.html
             const newUrl = new URL(request.url);
             newUrl.pathname = '/player/index.html';
             const modifiedRequest = new Request(newUrl, request);
             return env.ASSETS.fetch(modifiedRequest);
         }
 
-        // 其他所有请求（CSS、JS、图片等）使用默认行为
+        // 其他所有请求使用默认行为
         return env.ASSETS.fetch(request);
     }
 };
